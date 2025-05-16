ea784dab2d1a58267afed5a8928db9818a152519
Leon <qin168298@gmail.com>
2025-05-16 23:04:05 +0800

    添加了安装依赖文件，以及修改了默认配置

--- app/config.py
+++ app/config.py
@@ -6,13 +6,13 @@ Simple configuration for the build_index_v2.py
 class Config:
     # TBC RPC配置
     TBC_RPC_URL = "http://localhost:8332"
-    TBC_RPC_AUTH = ("user", "password")  # 替换为实际的用户名和密码
+    TBC_RPC_AUTH = ("tbcuser", "randompasswd")  # 替换为实际的用户名和密码
 
     # MySQL数据库配置
     MYSQL_HOST = "localhost"
     MYSQL_PORT = 3306
     MYSQL_USER = "root"
-    MYSQL_PASS = "password"  # 替换为实际的密码
+    MYSQL_PASS = "mysql149"  # 替换为实际的密码
     MYSQL_DB = "TBC20721"
     
     # AWS S3配置
